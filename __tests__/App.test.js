e219839c60fe50b4e5d835763cd86ca6bbbb3613
Tiago Oliveira <tiagonline@gmail.com>
2018-04-23 13:18:00 -0300

    Update App.test.js

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -10,3 +10,10 @@ describe('Testing App Component', () => {
         expect(wrapper).toMatchSnapshot();
     });
 });
+
+
+//Shallow Rendering
+//O shallow rendering renderiza apenas o componente, sem seus componentes filhos. 
+//Então, caso você mude algo em um componente filho, isso não irá modificar a renderização rasa 
+//(traduzindo para o português) do seu componente. Ou um bug, que seja introduzido em algum componente filho, 
+//não irá quebrar seus testes. E também, não necessita de nenhum pedaço do DOM.
